2303cb4cea860423b8159148319b407d01b483f2
Oliver Thor <oliverthor@icloud.com>
2023-05-17 12:19:42 -0400

    Initial Commit

---
+++ ReadAI/ReadAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadAIApp.swift
+//  ReadAI
+//
+//  Created by Oliver on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
